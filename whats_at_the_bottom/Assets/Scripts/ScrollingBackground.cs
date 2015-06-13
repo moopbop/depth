1b9c9c29cd59107d50939cf0da53e2523bc572e3
Patrick Bartlett <moopbop@gmail.com>
2015-06-13 13:59:55 -0400

    Implemented scrolling background

---
+++ whats_at_the_bottom/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Source: https://unity3d.com/learn/tutorials/modules/beginner/live-training-archive/2d-scrolling-backgrounds
+public class ScrollingBackground : MonoBehaviour
+{
+	public float scrollSpeed;
+	
+	void Update()
+	{
+		float y = Mathf.Repeat(Time.time * scrollSpeed, 1);
+		Vector2 offset = new Vector2(0, y);
+		this.GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
