87c66f54a4a93b004f03d9b26884d58aacd5e008
Jacob Mangia <tucker2134@gmail.com>
2015-06-13 22:29:07 -0400

    MainMenu

---
+++ whats_at_the_bottom/Assets/Scripts/MainMenuBackground.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBackground : MonoBehaviour {
+		
+	public float scrollSpeed;
+
+	void Update () {
+		float currentOffsetY = this.GetComponent<Renderer> ().sharedMaterial.GetTextureOffset ("_MainTex").y;
+		float y = Time.deltaTime * (scrollSpeed);
+		Vector2 offset = new Vector2(0, currentOffsetY + y);
+		this.GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
