9a47cf39a5cf964b87eb8561b41167a84dfea5d3
Jacob Mangia <tucker2134@gmail.com>
2015-06-13 22:29:34 -0400

    MainMenu

---
+++ whats_at_the_bottom/Assets/Scripts/Buttons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Buttons : MonoBehaviour {
+
+	public int scene;
+	
+	public void clickededed(){
+
+		if (scene != 4)
+			Application.LoadLevel (scene);
+		else
+			Application.Quit ();
+
+	}
+
+}
